c097f2d0cec21695606e81560fec046a146f1c2b
ocin-rye <contact@nicobryan.com>
2019-05-11 05:26:26 -0400

    Update todolist tests

--- src/components/todoList/TodoList.test.js
+++ src/components/todoList/TodoList.test.js
@@ -6,6 +6,8 @@ import TodoList from './index';
 
 describe('<TodoList />', () => {
   let todoList;
+  const mockToggleCompleted = jest.fn();
+  const mockDelete = jest.fn();
 
   const testTodoList = [
     {
@@ -30,14 +32,28 @@ describe('<TodoList />', () => {
 
   it('renders initial state without crashing', () => {
     let todoListFilter = 'All';
-    todoList = shallow(<TodoList todoList={testTodoList} todoListFilter={todoListFilter} />);
+    todoList = shallow(
+      <TodoList
+        todoList={testTodoList}
+        todoListFilter={todoListFilter}
+        toggleTodoCompletion={mockToggleCompleted}
+        deleteTodo={mockDelete}
+      />,
+    );
 
     expect(toJson(todoList)).toMatchSnapshot();
   });
 
   it('renders updated todo list', () => {
     let todoListFilter = 'All';
-    todoList = shallow(<TodoList todoList={testTodoList} todoListFilter={todoListFilter} />);
+    todoList = shallow(
+      <TodoList
+        todoList={testTodoList}
+        todoListFilter={todoListFilter}
+        toggleTodoCompletion={mockToggleCompleted}
+        deleteTodo={mockDelete}
+      />,
+    );
 
     todoList.setProps({ todoList: [] });
 
@@ -49,46 +65,86 @@ describe('<TodoList />', () => {
   describe('renders Header', () => {
     it('renders empty array todo list header', () => {
       let todoListFilter = 'All';
-      todoList = shallow(<TodoList todoList={testTodoListEmpty} todoListFilter={todoListFilter} />);
+      todoList = shallow(
+        <TodoList
+          todoList={testTodoListEmpty}
+          todoListFilter={todoListFilter}
+          toggleTodoCompletion={mockToggleCompleted}
+          deleteTodo={mockDelete}
+        />,
+      );
 
       expect(todoList.find('.header').text()).toEqual('No Todos');
     });
 
     it('renders empty array active todo list header', () => {
       let todoListFilter = 'Active';
-      todoList = shallow(<TodoList todoList={testTodoListEmpty} todoListFilter={todoListFilter} />);
+      todoList = shallow(
+        <TodoList
+          todoList={testTodoListEmpty}
+          todoListFilter={todoListFilter}
+          toggleTodoCompletion={mockToggleCompleted}
+          deleteTodo={mockDelete}
+        />,
+      );
 
       expect(todoList.find('.header').text()).toEqual('No Active Todos');
     });
 
     it('renders empty array completed todo list header', () => {
       let todoListFilter = 'Completed';
-      todoList = shallow(<TodoList todoList={testTodoListEmpty} todoListFilter={todoListFilter} />);
+      todoList = shallow(
+        <TodoList
+          todoList={testTodoListEmpty}
+          todoListFilter={todoListFilter}
+          toggleTodoCompletion={mockToggleCompleted}
+          deleteTodo={mockDelete}
+        />,
+      );
 
       expect(todoList.find('.header').text()).toEqual('No Completed Todos');
     });
 
     it('renders populated todo list header', () => {
       let todoListFilter = 'All';
-      todoList = shallow(<TodoList todoList={testTodoList} todoListFilter={todoListFilter} />);
+      todoList = shallow(
+        <TodoList
+          todoList={testTodoList}
+          todoListFilter={todoListFilter}
+          toggleTodoCompletion={mockToggleCompleted}
+          deleteTodo={mockDelete}
+        />,
+      );
 
       expect(todoList.find('.header').text()).toEqual('All Todos: 3');
     });
 
     it('renders populated active todo list header', () => {
       let todoListFilter = 'Active';
-      todoList = shallow(<TodoList todoList={testTodoList} todoListFilter={todoListFilter} />);
+      todoList = shallow(
+        <TodoList
+          todoList={testTodoList}
+          todoListFilter={todoListFilter}
+          toggleTodoCompletion={mockToggleCompleted}
+          deleteTodo={mockDelete}
+        />,
+      );
 
       expect(todoList.find('.header').text()).toEqual('Active Todos: 2');
     });
 
     it('renders populated completed todo list header', () => {
       let todoListFilter = 'Completed';
-      todoList = shallow(<TodoList todoList={testTodoList} todoListFilter={todoListFilter} />);
+      todoList = shallow(
+        <TodoList
+          todoList={testTodoList}
+          todoListFilter={todoListFilter}
+          toggleTodoCompletion={mockToggleCompleted}
+          deleteTodo={mockDelete}
+        />,
+      );
 
       expect(todoList.find('.header').text()).toEqual('Completed Todos: 1');
     });
   });
-
-  describe('handles input submit', () => {});
 });
